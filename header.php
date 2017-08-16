38e0a3ae034022a28529c304761a41116d4a3d15
Doodler46 <doodler46@mac.com>
2017-08-16 16:43:08 -0500

    header stylesheet not loading

--- header.php
+++ header.php
@@ -7,17 +7,25 @@
 	<link href="//www.google-analytics.com" rel="dns-prefetch">	
   <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
   <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css">
-  <link href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css">
-  <link href="<?php echo get_template_directory_uri(); ?>/css/wh.css">
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 
 	<?php wp_head(); ?>
+	<link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/wh.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -25,14 +33,14 @@
 <!-- Blue contact band -->
 	<div class="blue-row blue-band">
 		<div class="container">
-			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-9">
+			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-9"><!--schedule div-->
   			<a href="https://www.schedulicity.com/scheduling/WHCB4H"><i class="fa fa-calendar" aria-hidden="true"></i> Make an appointment</a>
 			</div><!--column (schedule) div-->
 			<div class="col-lg-2 col-md-3 col-sm-3 col-xs-3" style="float:right">
 				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12"><!--phone div-->
 					<a href="tel:651-459-2000"><i class="fa fa-phone" aria-hidden="true"></i></a>
 				</div><!--column (phone) div-->
-				<div class="col-lg-9 col-md-9 col-sm-9 hidden-xs">
+				<div class="col-lg-9 col-md-9 col-sm-9 hidden-xs"><!--number div-->
 					<a href="tel:651-459-2000">651.459.2000</a>
 				</div><!--column (number) div-->
 			</div><!--column (call) div-->
@@ -72,11 +80,11 @@
     	<div class="row">
 		  	<div class="col-lg-12">
  	   			<center><img class="img-responsive" src="img/WH_logo.png" alt="logo"/> </center>
-	      </div>
-	    </div>
-  	</div>
+	      </div><!--col div-->
+	    </div><!--row div-->
+  	</div><!--container div-->
   	<div class="logo">
     	<img class="img-responsive" src="img/501295459_hero_large.jpg" alt="hero"/>
-  	</div>
+  	</div><!--logo div-->
 	</header>
 	
--- index.php
+++ index.php
@@ -1,19 +1,44 @@
 <?php get_header(); ?>
-	<main>
-      <link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
-        <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css">
-        <link href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css">
-        <link href="<?php echo get_template_directory_uri(); ?>/css/wh.css">
-		<section>
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
-			<?php get_template_part('loop'); ?>
-			<?php get_template_part('pagination'); ?>
-		</section>
-	</main>
+
+<head>
+  <link href="//www.google-analytics.com" rel="dns-prefetch">
+  <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+  <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/wh.css">
+</head>
+
+<main>
+
+
+	<section>
+		<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+		<?php get_template_part('loop'); ?>
+		<?php get_template_part('pagination'); ?>
+	</section>
+
+<!--  Benifits section -->
+<section>
+  <div class="row green-band">
+      <div class="container">
+        <div class="col-lg-1 page-header col-xs-3">
+          <center>
+            <img class="img-responsive" src="img/spine.png" style="padding-top:25px" alt="spine"/>
+          </center>
+        </div>
+        <div class="col-lg-11 page-header col-xs-9">
+          <h1></h1>
+          <p></p>
+          <button type="button" class="btn" style="padding-left:15px; padding-right:15px"><a href="http://www.wholehealthmn.com/benefits.html">Learn how we can help</a></button>
+        </div>
+      </div><!--container div-->
+  </div><!--green band row-->
+</section>
+
+</main>
 
 <?php get_sidebar(); ?>
 

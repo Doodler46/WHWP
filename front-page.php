c15d232d52232125a1fa1593c2ad80016a639787
Niki Brown <nbrown@fitbit.com>
2017-08-25 07:30:22 -0400

    add repeater

--- front-page.php
+++ front-page.php
@@ -6,12 +6,26 @@
     <div class="row">
       <div class="col-lg-12">
 
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<!-- Start WordPress loop -->
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-	    			<?php the_content(); ?>
+			<!-- Main Content -->
+			<?php the_content(); ?>
+			
+			<!-- check if the repeater field has rows of data (quote content custom fields) -->
+			<?php if( have_rows('quote') ): ?>
+				<!-- loop through the rows of data -->
+				<?php while ( have_rows('quote') ) : the_row(); ?>
+					<!-- display a sub field value -->
+					<?php the_sub_field('quote_name'); ?>
+					<?php the_sub_field('quote_text'); ?>
+					<?php the_sub_field('quote_image'); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
 
-<?php endwhile; endif; ?> 
-
+		<!-- End WordPress loop -->
+		<?php endwhile; endif; ?> 
+	
       </div>    
     </div>
   </div><!--jumbotron-->

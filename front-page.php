6f409f01aa92b673e176af6a67ebeab06cc8403b
Niki Brown <nbrown@c.fitbit.com>
2017-08-31 18:19:46 -0400

    add cycle structure divs

--- front-page.php
+++ front-page.php
@@ -16,35 +16,43 @@
 				<div class="row">
 					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 						<!-- check if the repeater field has rows of data (quote content custom fields) -->
-						<?php if( have_rows('quote_left') ): ?>
-							<!-- loop through the rows of data -->
-							<?php while ( have_rows('quote_left') ) : the_row(); ?>
-								<!-- display a sub field value -->
-								<p>
-									<?php the_sub_field('quote_text_left'); ?>
-								</p>
-								<p>
-									<?php the_sub_field('quote_name_left'); ?>
-								</p>
-								<img src="<?php the_sub_field('quote_image_left'); ?>" alt="<?php the_sub_field('quote_name'); ?>" />
-							<?php endwhile; ?>
-						<?php endif; ?>
+						<div class="cycle-slideshow" data-cycle-slides="> .slide">
+							<?php if( have_rows('quote_left') ): ?>
+								<!-- loop through the rows of data -->
+								<?php while ( have_rows('quote_left') ) : the_row(); ?>
+									<div class="slide">
+										<!-- display a sub field value -->
+										<p>
+											<?php the_sub_field('quote_text_left'); ?>
+										</p>
+										<p>
+											<?php the_sub_field('quote_name_left'); ?>
+										</p>
+										<img src="<?php the_sub_field('quote_image_left'); ?>" alt="<?php the_sub_field('quote_name'); ?>" />
+									</div><!--/.slide-->
+								<?php endwhile; ?>
+							<?php endif; ?>
+						</div><!--/.cycle-sldieshow-->
 					</div>	<!--/.col-->
 					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 						<!-- check if the repeater field has rows of data (quote content custom fields) -->
-						<?php if( have_rows('quote_right') ): ?>
-							<!-- loop through the rows of data -->
-							<?php while ( have_rows('quote_right') ) : the_row(); ?>
-								<!-- display a sub field value -->
-								<p>
-									<?php the_sub_field('quote_text_right'); ?>
-								</p>
-								<p>
-									<?php the_sub_field('quote_name_right'); ?>
-								</p>
-								<img src="<?php the_sub_field('quote_image_right'); ?>" alt="<?php the_sub_field('quote_name'); ?>" />
-							<?php endwhile; ?>
-						<?php endif; ?>
+						<div class="cycle-slideshow" data-cycle-slides="> .slide">
+							<?php if( have_rows('quote_right') ): ?>
+								<!-- loop through the rows of data -->
+								<?php while ( have_rows('quote_right') ) : the_row(); ?>
+									<div class="slide">
+										<!-- display a sub field value -->
+										<p>
+											<?php the_sub_field('quote_text_right'); ?>
+										</p>
+										<p>
+											<?php the_sub_field('quote_name_right'); ?>
+										</p>
+										<img src="<?php the_sub_field('quote_image_right'); ?>" alt="<?php the_sub_field('quote_name'); ?>" />
+									</div><!--/.slide-->
+								<?php endwhile; ?>
+							<?php endif; ?>
+						</div><!--/.cycle-slideshow-->
 					</div>	<!--/.col-->
 				</div><!--/.row-->
 			</setion>

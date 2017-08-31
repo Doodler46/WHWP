f6bfe6ac1a20d2cd7904e0cdac38611297764704
Niki Brown <nbrown@c.fitbit.com>
2017-08-31 18:38:15 -0400

    remove srcset

--- functions.php
+++ functions.php
@@ -32,6 +32,9 @@ if (function_exists('add_theme_support'))
     add_image_size('small', 120, '', true); // Small Thumbnail
     add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
+    // disable srcset on frontend
+    add_filter('max_srcset_image_width', create_function('', 'return 1;'));
+
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     /*add_theme_support('custom-background', array(
 	'default-color' => 'FFF',

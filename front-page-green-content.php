9365044d89980726991ea8b777225db59032cadd
Doodler46 <doodler46@mac.com>
2017-09-17 09:03:16 -0500

    images added and styles updated

---
+++ front-page-green-content.php
@@ -0,0 +1,78 @@
+<?php get_header(); ?>
+
+<section class="green-band">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <?php the_field('green_benefits')?>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- About us section -->
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+
+		<!-- Start WordPress loop -->
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<!-- Main Content -->
+			<?php the_content(); ?>
+
+			<setion class="testimonial">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<!-- check if the repeater field has rows of data (quote content custom fields) -->
+						<div class="cycle-slideshow quote-left" data-cycle-slides="> .slide">
+							<?php if( have_rows('quote_left') ): ?>
+								<!-- loop through the rows of data -->
+								<?php while ( have_rows('quote_left') ) : the_row(); ?>
+									<div class="slide">
+										<!-- display a sub field value -->
+										<img src="<?php the_sub_field('quote_image_left'); ?>" alt="<?php the_sub_field('quote_name'); ?>" />
+										<p>
+											<?php the_sub_field('quote_text_left'); ?>
+										</p>
+										<p>
+											<?php the_sub_field('quote_name_left'); ?>
+										</p>
+										
+									</div><!--/.slide-->
+								<?php endwhile; ?>
+							<?php endif; ?>
+						</div><!--/.cycle-sldieshow-->
+					</div>	<!--/.col-->
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<!-- check if the repeater field has rows of data (quote content custom fields) -->
+						<div class="cycle-slideshow quote-right" data-cycle-slides="> .slide">
+							<?php if( have_rows('quote_right') ): ?>
+								<!-- loop through the rows of data -->
+								<?php while ( have_rows('quote_right') ) : the_row(); ?>
+									<div class="slide">
+										<!-- display a sub field value -->
+										<img src="<?php the_sub_field('quote_image_right'); ?>" alt="<?php the_sub_field('quote_name'); ?>" />
+										<p>
+											<?php the_sub_field('quote_text_right'); ?>
+										</p>
+										<p>
+											<?php the_sub_field('quote_name_right'); ?>
+										</p>
+										
+									</div><!--/.slide-->
+								<?php endwhile; ?>
+							<?php endif; ?>
+						</div><!--/.cycle-slideshow-->
+					</div>	<!--/.col-->
+				</div><!--/.row-->
+			</setion>
+			
+		<!-- End WordPress loop -->
+		<?php endwhile; endif; ?> 
+	
+    </div>    
+  </div>
+</div><!--container-->
+	           
+<?php get_footer(); ?>
